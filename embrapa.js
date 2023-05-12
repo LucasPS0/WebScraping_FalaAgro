e7c341f2c046dda11c048ff4302aa82e382b5824
LCourtneey <112607752+LCourtneey@users.noreply.github.com>
2023-05-12 12:37:53 -0300

    Added comments on the code

--- embrapa.js
+++ embrapa.js
@@ -1,28 +1,39 @@
+// Importa as bibliotecas necessárias
 const cheerio = require("cheerio");
 const axios = require("axios");
 const { MongoClient } = require("mongodb");
 const cron = require("node-cron");
 
 (async () => {
+  // Define a URL do site a ser raspado
   const url = "https://www.embrapa.br/busca-de-noticias";
 
+  // Define a URI do banco de dados MongoDB
   const uri = "mongodb://127.0.0.1/";
   const client = new MongoClient(uri);
 
+  // Função para raspar as notícias do site Embrapa
   const scrapEmbrapa = async () => {
     try {
+      // Conecta ao banco de dados MongoDB
       await client.connect();
 
+      // Seleciona o banco de dados e a coleção
       const database = client.db("noticia");
       const collection = database.collection("noticias");
 
+      // Faz uma requisição GET para obter o HTML da página
       const { data } = await axios.get(url);
 
+      // Carrega o HTML obtido com a biblioteca Cheerio
       const $ = cheerio.load(data);
 
+      // Inicializa um array para armazenar as notícias
       const noticias = [];
 
+      // Itera sobre os elementos da página que contêm as notícias
       for (const element of $(".table-data .conteudo")) {
+        // Extrai as informações da notícia
         const titulo = $(element).find(".titulo").text().trim();
         const resumo = $(element).find(".detalhes p").text().trim();
         const dataPublicacao = $(element)
@@ -30,42 +41,19 @@ const cron = require("node-cron");
           .text();
         const link = $(element).find("a").attr("href").replace(/\?.*/, "");
 
+        // Faz uma nova requisição GET para obter o HTML da notícia individual
         const { data: noticiaData } = await axios.get(link);
         const $noticia = cheerio.load(noticiaData);
-        // encontra o texto da notícia
 
-
-        
+        // Extrai o texto da notícia e ajusta os links das imagens
         const textoNoticia = $noticia(".texto-noticia").html().replace(/src="\/documents/g, 'src="https://www.embrapa.br/documents');
 
-        
-        
-    
-  
-
-        // encontra a imagem principal
-        const imagemPrincipal = $noticia(".imagem-principal img[src]").attr(
-          "data-src"
-        );
-
+        // Extrai a imagem principal e suas informações
+        const imagemPrincipal = $noticia(".imagem-principal img[src]").attr("data-src");
         const imagemCompleta = "https://www.embrapa.br/" + imagemPrincipal;
-      
-        const legendaimagemPrincipal = $noticia(
-          ".legenda-imagem-principal"
-        ).text();
-      
-
-        const fonteimagemPrincipal = $noticia(
-          ".fonte-imagem-principal"
-        ).text();
-     
-
+        const legendaimagemPrincipal = $noticia(".legenda-imagem-principal").text();
+        const fonteimagemPrincipal = $noticia(".fonte-imagem-principal").text();
         const autor = $noticia(".autor").text();
-       
-
-
-
-        
 
         // Verifica se a notícia já existe no banco de dados
         const existingNoticia = await collection.findOne({ link });
@@ -81,8 +69,6 @@ const cron = require("node-cron");
             legendaimagemPrincipal: legendaimagemPrincipal,
             fonteimagemPrincipal: fonteimagemPrincipal,
             autor: autor,
-          
-
           };
 
           // Adiciona a imagem principal, se existir
@@ -104,20 +90,18 @@ const cron = require("node-cron");
             legendaimagemPrincipal: existinglegendaimagemPrincipal,
             fonteimagemPrincipal: existingfonteimagemPrincipal,
             autor: existingautor,
-         
-
           } = existingNoticia;
 
           const camposModificados =
-          existingTitulo !== titulo ||
-          existingResumo !== resumo ||
-          existingTextoNoticia !== textoNoticia||
-          existingImagemCompleta !== imagemCompleta ||
-          existingdataPublicacao !== dataPublicacao ||
-          existinglegendaimagemPrincipal !== legendaimagemPrincipal ||
-          existingfonteimagemPrincipal !== fonteimagemPrincipal ||
-          existingautor !== autor 
-          
+            existingTitulo !== titulo ||
+            existingResumo !== resumo ||
+            existingTextoNoticia !== textoNoticia ||
+            existingImagemCompleta !== imagemCompleta ||
+            existingdataPublicacao !== dataPublicacao ||
+            existinglegendaimagemPrincipal !== legendaimagemPrincipal ||
+            existingfonteimagemPrincipal !== fonteimagemPrincipal ||
+            existingautor !== autor;
+
           if (camposModificados) {
             // Se houve alteração nos campos, atualiza a notícia no banco de dados
             await collection.updateMany(
@@ -136,14 +120,10 @@ const cron = require("node-cron");
               }
             );
 
-            console.log(
-              `Notícia "${titulo}" foi atualizada no banco de dados.`
-            );
+            console.log(`Notícia "${titulo}" foi atualizada no banco de dados.`);
           } else {
             // Se não houve alteração nos campos, não faz nada
-            console.log(
-              `Notícia "${titulo}" já existe no banco de dados e não precisa ser atualizada.`
-            );
+            console.log(`Notícia "${titulo}" já existe no banco de dados e não precisa ser atualizada.`);
           }
         }
       }
@@ -152,10 +132,12 @@ const cron = require("node-cron");
     } catch (error) {
       console.log(error);
     } finally {
+      // Fecha a conexão com o banco de dados
       await client.close();
     }
   };
 
+  // Agenda a função scrapEmbrapa para ser executada a cada 10 segundos
   cron.schedule("*/10 * * * * *", async () => {
     await scrapEmbrapa();
   });
