341ca9f7e74111c80246e498b75b236ced1eca52
unknown <ruanlucenaramos@gmail.com>
2023-05-12 14:24:55 -0300

    integrando banco mongo com scraping da globo rural

--- GloboRural.js
+++ GloboRural.js
@@ -1,104 +1,66 @@
-const puppeteer = require('puppeteer');
-const { MongoClient } = require('mongodb');
-const cron = require('node-cron');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://127.0.0.1/";
+const client = new MongoClient(uri);
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+  const url = "https://globorural.globo.com/ultimas-noticias/";
 
+  const scrapGloboRural = async () => {
+    const dataGloboRural = {};
 
+    async function getHtml() {
+      const { data: html } = await axios.get(url);
+      return html;
+    }
 
-  async function buscarNoticias() {
-    await page.goto('https://g1.globo.com/economia/agronegocios/globo-rural/');
-    await page.waitForSelector('.feed-post-body');
+    const response = await getHtml();
+    const $ = cheerio.load(response);
+    $(".feed-post-body").each((index, element) => {
+      const title = $(element).find(".feed-post-link").text();
+      const resume = $(element).find(".feed-post-body-resumo").text();
+      const link = $(element).find(".feed-post-body a").attr("href");
+      const publication = $(element).find(".feed-post-datetime").text();
+      const theme = $(element).find(".feed-post-metadata-section").text();
+      const image = $(element).find(".feed-post-figure-link img").attr("src");
 
-    // Rola ate o final da pagina
-    await page.evaluate(async () => {
-      await new Promise((resolve, reject) => {
-        let totalHeight = 0;
-        const distance = 100;
-        const timer = setInterval(() => {
-          const scrollHeight = document.body.scrollHeight;
-          window.scrollBy(0, distance);
-          totalHeight += distance;
-
-          if (totalHeight >= scrollHeight) {
-            clearInterval(timer);
-            resolve();
-          }
-        }, 100);
-      });
+      // Archive data in an object
+      dataGloboRural[index] = { title, resume, link, publication, theme, image };
     });
 
-    // Coleta as noticias
-    const noticias = await page.evaluate(() => {
-      const newsElementos = Array.from(document.querySelectorAll('.feed-post-body'));
-      return newsElementos.map(newsElementos => {
-        const tituloElementos = newsElementos.querySelector('a.feed-post-link');
-        const titulo = tituloElementos.textContent;
-        const link = tituloElementos.href;
-        const resumoElemento = newsElementos.querySelector('.feed-post-body-resumo');
-        const resumo = resumoElemento.textContent;
-        return { titulo, resumo, link };
-      });
+    fs.writeFile("globoData.json", JSON.stringify(dataGloboRural), (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
     });
 
-    // Conecta ao MongoDB
-    const uri = 'mongodb://127.0.0.1/';
-    const client = new MongoClient(uri);
+    return dataGloboRural;
+  };
 
+  async function insertData(data) {
     try {
       await client.connect();
+      const database = client.db("noticia");
+      const collection = database.collection("noticias");
 
-      const database = client.db('local');
-      const collection = database.collection('Noticias');
-
-
-
-
-      for (const noticia of noticias) {
-        const existingNoticia = await collection.findOne({ link: noticia.link });
-        if (existingNoticia) {
-          // Atualiza a noticia (caso tenha mudado o conteudo)
-          const result = await collection.updateOne({ link: noticia.link }, { $set: { titulo: noticia.titulo, resumo: noticia.resumo } });
-          console.log(`${result.modifiedCount} document(s) updated.`);
-        } else {
-          // Insere nova noticia (Caso o link ainda nao exista no Banco de dados)
-          const result = await collection.insertOne(noticia);
-          console.log(`${result.insertedCount} document(s) inserted.`);
-        }
-      }
-
-      for (const noticia of noticias) {
-        // Abre a página da notícia
-        await page.goto(noticia.link);
-
-        // Espera pelo seletor .content-text__container
-        await page.waitForSelector('.mc-article-body p.content-text__container');
-
-        // Extrai o texto da notícia
-        const texto = await page.evaluate(() => {
-          const textoElementos = Array.from(document.querySelectorAll('.mc-article-body p.content-text__container '));
-          return textoElementos.map(textoElemento => textoElemento.textContent).join('\n');
-        });
-
-        // Atualiza a notícia com o texto
-        const result = await collection.updateOne({ link: noticia.link }, { $set: { texto } });
-        console.log(`${result.modifiedCount} document(s) updated.`);
-      }
-
-
+      const result = await collection.insertOne(data);
+      console.log("Dados inseridos no MongoDB com sucesso:", result.insertedId);
     } catch (err) {
-      console.error(err);
+      console.error("Erro ao inserir os dados no MongoDB:", err);
     } finally {
-      // Fehca a conexao
-      client.close();
+      await client.close();
     }
   }
 
-  cron.schedule('*/5 * * * *', async () => {
-    await buscarNoticias();
+  const dataGloboRural = await scrapGloboRural();
+
+  fs.writeFile("globoData.json", JSON.stringify(dataGloboRural), (err) => {
+    if (err) throw err;
+    console.log("O arquivo foi salvo!");
+
+    // Chama a função para inserir os dados no MongoDB
+    insertData(dataGloboRural);
   });
-
-
 })();
