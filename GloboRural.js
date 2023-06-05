80be4a514e85e253332fa55c504f34785ba084c5
LCourtneey <112607752+LucasPS0@users.noreply.github.com>
2023-06-05 01:06:00 -0300

    Revert "Scraping Globorural adicionado"
    This reverts commit 40e09b9339284d72e76a272a9616aadc33fc4137.

--- GloboRural.js
+++ GloboRural.js
@@ -2,8 +2,6 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
 const { MongoClient } = require("mongodb");
-const moment = require('moment');
-
 
 const uri = "mongodb+srv://lucaspsilva:35253030@courtneysdata.vjqs1cs.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
@@ -22,35 +20,26 @@ const client = new MongoClient(uri);
     async function getCompleteNews(link) {
       const { data: html } = await axios.get(link);
       const $ = cheerio.load(html);
-      const textoNoticia = $("article").html().trim();
-      const dataPublicacaoFormatted = moment($(".content-publication-data__updated").text(), 'DD/MM/YYYY HH:mm').format('DD/MM/YYYY');
-      const dataParts = dataPublicacaoFormatted.split('/');
-      const dataPublicacao = new Date(Number(dataParts[2]), Number(dataParts[1]) - 1, Number(dataParts[0]));
-      const titulo = $(".title").text();
-      
-    
-      return { textoNoticia, dataPublicacao, titulo };
+      const textoNoticia = $("article").text().trim();
+   
+      return textoNoticia;
     }
-    
 
     const response = await getHtml();
-const $ = cheerio.load(response);
-const feedPostBody = $(".feed-post-body").toArray();
+    const $ = cheerio.load(response);
+    const feedPostBody = $(".feed-post-body").toArray();
 
-for (const element of feedPostBody) {
-  const resumo = $(element).find(".feed-post-body-resumo").text();
-  const link = $(element).find(".feed-post-body a").attr("href");
-  const imagemCompleta = $(element).find(".feed-post-figure-link img").attr("src");
-  const fonte = "Fonte: Globo Rural";
+    for (const element of feedPostBody) {
+      const titulo = $(element).find(".feed-post-link").text();
+      const resumo = $(element).find(".feed-post-body-resumo").text();
+      const link = $(element).find(".feed-post-body a").attr("href");
+      const dataPublicacao = $(element).find(".feed-post-datetime").text();
+      const imagemCompleta = $(element).find(".feed-post-figure-link img").attr("src");
+      const fonte = "Fonte: Globo Rural"
+      const textoNoticia = await getCompleteNews(link);
 
-  const { textoNoticia, dataPublicacao, titulo } = await getCompleteNews(link);
-
-  const dataPublicacaoFormatted = moment(dataPublicacao).format('DD/MM/YYYY');
-  const resumoConcatenado = resumo + " - Data de publicação: " + dataPublicacaoFormatted;
-
-  dataGloboRural.push({ titulo, resumo: resumoConcatenado, link, imagemCompleta, textoNoticia, dataPublicacao, fonte });
-}
-
+      dataGloboRural.push({ titulo, resumo, link, dataPublicacao, imagemCompleta, textoNoticia, fonte });
+    }
 
     fs.writeFile("globoData.json", JSON.stringify(dataGloboRural), (err) => {
       if (err) throw err;
