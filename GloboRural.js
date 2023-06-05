110126ab5826a9f99e52f10cc60b55f9dba6f123
LCourtneey <112607752+LucasPS0@users.noreply.github.com>
2023-06-05 01:02:44 -0300

    Revert "att"
    This reverts commit 96c5189ef55621b50a6ff75e5eca5802efbff1da.

--- GloboRural.js
+++ GloboRural.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require("fs");
 const { MongoClient } = require("mongodb");
-const moment = require("moment");
-const cron = require("node-cron");
+const moment = require('moment');
 
-const uri =
-  "mongodb+srv://lucaspsilva:35253030@courtneysdata.vjqs1cs.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = "mongodb+srv://lucaspsilva:35253030@courtneysdata.vjqs1cs.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 (async () => {
@@ -23,52 +23,39 @@ const client = new MongoClient(uri);
       const { data: html } = await axios.get(link);
       const $ = cheerio.load(html);
       const textoNoticia = $("article").html().trim();
-      const dataPublicacaoFormatted = moment(
-        $(".content-publication-data__updated").text(),
-        "DD/MM/YYYY HH:mm"
-      ).format("DD/MM/YYYY");
-      const dataParts = dataPublicacaoFormatted.split("/");
-      const dataPublicacao = new Date(
-        Number(dataParts[2]),
-        Number(dataParts[1]) - 1,
-        Number(dataParts[0])
-      );
+      const dataPublicacaoFormatted = moment($(".content-publication-data__updated").text(), 'DD/MM/YYYY HH:mm').format('DD/MM/YYYY');
+      const dataParts = dataPublicacaoFormatted.split('/');
+      const dataPublicacao = new Date(Number(dataParts[2]), Number(dataParts[1]) - 1, Number(dataParts[0]));
       const titulo = $(".title").text();
-
+      
+    
       return { textoNoticia, dataPublicacao, titulo };
     }
+    
 
     const response = await getHtml();
-    const $ = cheerio.load(response);
-    const feedPostBody = $(".feed-post-body").toArray();
+const $ = cheerio.load(response);
+const feedPostBody = $(".feed-post-body").toArray();
 
-    for (const element of feedPostBody) {
-      const resumo = $(element).find(".feed-post-body-resumo").text();
-      const link = $(element).find(".feed-post-body a").attr("href");
-      const imagemCompleta = $(element)
-        .find(".feed-post-figure-link img")
-        .attr("src");
-      const fonte = "Fonte: Globo Rural";
+for (const element of feedPostBody) {
+  const resumo = $(element).find(".feed-post-body-resumo").text();
+  const link = $(element).find(".feed-post-body a").attr("href");
+  const imagemCompleta = $(element).find(".feed-post-figure-link img").attr("src");
+  const fonte = "Fonte: Globo Rural";
 
-      const { textoNoticia, dataPublicacao, titulo } = await getCompleteNews(
-        link
-      );
+  const { textoNoticia, dataPublicacao, titulo } = await getCompleteNews(link);
 
-      const dataPublicacaoFormatted =
-        moment(dataPublicacao).format("DD/MM/YYYY");
-      const resumoConcatenado =
-        resumo + " - Data de publicação: " + dataPublicacaoFormatted;
+  const dataPublicacaoFormatted = moment(dataPublicacao).format('DD/MM/YYYY');
+  const resumoConcatenado = resumo + " - Data de publicação: " + dataPublicacaoFormatted;
 
-      dataGloboRural.push({
-        titulo,
-        resumo: resumoConcatenado,
-        link,
-        imagemCompleta,
-        textoNoticia,
-        dataPublicacao,
-        fonte,
-      });
-    }
+  dataGloboRural.push({ titulo, resumo: resumoConcatenado, link, imagemCompleta, textoNoticia, dataPublicacao, fonte });
+}
+
+
+    fs.writeFile("globoData.json", JSON.stringify(dataGloboRural), (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
 
     return dataGloboRural;
   };
@@ -88,11 +75,9 @@ const client = new MongoClient(uri);
         }
 
         const result = await collection.insertOne(news);
-        console.log(
-          "Dados inseridos no MongoDB com sucesso:",
-          result.insertedId
-        );
+        console.log("Dados inseridos no MongoDB com sucesso:", result.insertedId);
       }
+
     } catch (err) {
       console.error("Erro ao inserir os dados no MongoDB:", err);
     } finally {
@@ -102,12 +87,11 @@ const client = new MongoClient(uri);
 
   const dataGloboRural = await scrapGloboRural();
 
-  // Chama a função para inserir os dados no MongoDB
-  insertData(dataGloboRural);
+  fs.writeFile("globoData.json", JSON.stringify(dataGloboRural), (err) => {
+    if (err) throw err;
+    console.log("O arquivo foi salvo!");
 
-  // Agendamento para executar o scraping e inserção dos dados a cada 10 segundos
-  cron.schedule("*/10 * * * * *", async () => {
-    const newData = await scrapGloboRural();
-    insertData(newData);
+    // Chama a função para inserir os dados no MongoDB
+    insertData(dataGloboRural);
   });
 })();
