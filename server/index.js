844a2f295b26dfa102f713a4cc1a98355aa761d0
Lucas Pedro <112607752+LucasPS0@users.noreply.github.com>
2023-11-19 22:55:43 -0300

    added some things

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const app = express();
